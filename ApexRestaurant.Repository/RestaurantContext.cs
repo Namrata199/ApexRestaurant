9f6c40134bb9bf8cecf24264928acaee1e33903c
Namrata199 <namru.mail@gmail.com>
2021-12-19 20:44:40 +0545

    added other models

--- ApexRestaurant.Repository/RestaurantContext.cs
+++ ApexRestaurant.Repository/RestaurantContext.cs
@@ -3,20 +3,28 @@ using ApexRestaurant.Repository.Domain;
 
 namespace ApexRestaurant.Repository
 {
-public class RestaurantContext : DbContext
-{
+    public class RestaurantContext : DbContext
+    {
+         public RestaurantContext(DbContextOptions<RestaurantContext> options) :base(options)
+    {
 
-public RestaurantContext(DbContextOptions<RestaurantContext> options) :
-base(options)
-{
+    }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Customer> Menus { get; set; }
+
+    public DbSet<Customer> Menu_Items { get; set; }
+
+    public DbSet<Customer> Ref_Staff_Roles { get; set; }
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    }
+
 }
 
-public DbSet<Customer> Customers { get; set; }
 
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-base.OnModelCreating(modelBuilder);
-}
-
-}
-}
